2c51b5257e38c96d05edea1bf580a241a7e36d35
jjung-developer <iamhojjung@gmail.com>
2021-02-14 18:30:23 +0900

    todoList완성

--- index.js
+++ index.js
@@ -1,21 +1,81 @@
-const todo = [];
+let todos = [];
 const todoList = document.querySelector("#todo-list");
 const addlistbtn = document.querySelector(".input-todo-button");
+const todoText = document.querySelector(".input-todo");
 
-const getUserstodo = () => {
-  const ListItem = document.createElement("li");
-  const todoText = document.querySelector(".input-todo").value;
-  //const tododelectBtn = document.createElement("button");
-  const todoobj = {
-    txt: todoText,
-    done: false,
-    id: todo.length + 1
-  };
-  
-  todo.push(todoobj);
-  console.log(todo);
-  //ListItem.appendChild(todoText);
-  //todoList.appendChild(ListItem);
+let new_id = 0;
+
+const makeElement = (todo) => {
+  const li = document.createElement("li");
+  const deletebtn = document.createElement("button");
+  const todotxt = document.createElement("p");
+  const doneicon = document.createElement("span");
+
+  deletebtn.textContent = "삭제";
+  doneicon.textContent = todo.done ? "O" : "X";
+  todotxt.textContent = todo.txt;
+
+  todotxt.addEventListener("click", () => {
+    toggleTodo(todo.id);
+  });
+
+  deletebtn.addEventListener("click", () => {
+    deleteTodo(todo.id);
+  });
+
+  li.appendChild(todotxt);
+  li.appendChild(deletebtn);
+  li.appendChild(doneicon);
+  todoList.appendChild(li);
 };
 
-addlistbtn.addEventListener("click", getUserstodo);
+const resetTodos = () => {
+  while (todoList.hasChildNodes()) {
+    todoList.removeChild(todoList.firstChild);
+  }
+};
+
+const deleteTodo = (id) => {
+  const deletedTodo = todos.filter((todo) => {
+    if (todo.id !== id) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  todos = deletedTodo;
+
+  printtodos();
+};
+
+const toggleTodo = (id) => {
+  const result = todos.map((todo) => {
+    if (todo.id === id) {
+      todo.done = !todo.done;
+    }
+    return todo;
+  });
+  todos = result;
+
+  printtodos();
+};
+
+const addTodo = () => {
+  const value = todoText.value;
+  const todoobj = {
+    txt: value,
+    done: false,
+    id: new_id++,
+  };
+  todos.push(todoobj);
+  printtodos();
+};
+
+const printtodos = () => {
+  resetTodos();
+  todos.forEach((todo) => {
+    makeElement(todo);
+  });
+};
+
+addlistbtn.addEventListener("click", addTodos);
